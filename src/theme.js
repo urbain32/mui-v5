0cfaa975b040a05b3b166bb7a9e595e47b47fba8
Mac <mac@Mac.local>
2022-11-11 15:57:46 +0200

    creating a theme provider

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#888',
+      light: 'skyBlue',
+    },
+    secondary: {
+      main: '#15b678',
+    },
+    otherColor: {
+      main: 'red',
+    },
+  },
+});
