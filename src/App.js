d31c8f6169da575b2637b7ddf7241aee28d3ec99
urbain32 <urbainq32@gmail.com>
2022-11-16 18:15:40 +0200

    #creating a parent prop that can be used everywhere

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Rightbar from './components/Rightbar';
-import Feed from './components/Feed';
-import { Box, Stack } from '@mui/material';
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import Feed from "./components/Feed";
+import { Box, Stack } from "@mui/material";
+import { useState } from "react";
 function App() {
+  const [like, setLike] = useState(0);
   return (
     <Box>
       {/* navbar */}
-      <Navbar />
+      <Navbar like={like} />
       <Stack direction='row' spacing={2} justifyContent='space-between'>
-      
         <Sidebar />
-        <Feed />
+        <Feed like={like} setLike={setLike} />
         <Rightbar />
       </Stack>
     </Box>
